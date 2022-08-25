79b1adf443a776a57aa8ae5343eaecc7754928c4
GustavoAns <gugasato9@live.com>
2022-08-25 07:38:42 -0300

    Iniciando projeto

---
+++ softeo-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as C from './App.styles'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const App = () => {
+  return (
+    <C.Container>
+      Inicio
+    </C.Container>
+  )
+}
+
+export default App
